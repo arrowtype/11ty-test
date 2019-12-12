bf9a6e54318176450684b0295e060506b0dd775b
Stephen Nixon <stephen@thundernixon.com>
2019-12-11 23:14:05 -0500

    setting up basic 11ty project

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+// module.exports = {
+//     dir: {
+//         input: "src",
+//         output: "dist"
+//         // relative to 'input' (these are inside 'src')
+//         includes: "_includes",
+//         layouts: "_layouts"
+//     },
+//     htmlTemplateEngine: "njk"
+// };
+
+module.exports = function(eleventyConfig) {
+  
+    eleventyConfig.addWatchTarget("./src/styles/");
+    // You can return your Config object (optional).
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            // relative to 'input' (these are inside 'src')
+            includes: "_includes",
+            layouts: "_layouts"
+        },
+        templateFormats: ['njk', 'md', 'scss', 'css', 'html', 'yml'],
+        htmlTemplateEngine: "njk"
+    };
+  };
+
+// module.exports = function(eleventyConfig) {
+// eleventyConfig.addWatchTarget("./src/scss/");
+// };
---
+++ README.md
@@ -0,0 +1,5 @@
+# Page header
+
+hello?
+
+oh nice?
